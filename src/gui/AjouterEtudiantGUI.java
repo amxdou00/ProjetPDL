cd631cbe7b4f79ef36d2337bd392935a88e54a92
NDEYE AMY DIAW <ndeye465@gmail.com>
2023-05-20 16:36:44 +0200

    interface ajout etudiant done

--- src/gui/AjouterEtudiantGUI.java
+++ src/gui/AjouterEtudiantGUI.java
@@ -66,37 +66,40 @@ public class AjouterEtudiantGUI {
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Ajout d'un étudiant");
-		lblNewLabel.setBounds(-14, 192, 720, 48);
+		lblNewLabel.setBounds(-70, 50, 720, 48);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 20));
 		panel.add(lblNewLabel);
 		
 		textFieldNom = new JTextField();
-		textFieldNom.setBounds(77, 260, 175, 30);
+		textFieldNom.setBounds(274, 127, 175, 30);
 		panel.add(textFieldNom);
 		textFieldNom.setColumns(10);
 		
 		textFieldPrenom = new JTextField();
 		textFieldPrenom.setColumns(10);
-		textFieldPrenom.setBounds(399, 260, 175, 30);
+		textFieldPrenom.setBounds(274, 180, 175, 30);
 		panel.add(textFieldPrenom);
 		
 		JLabel lblNewLabel_1 = new JLabel("Nom");
-		lblNewLabel_1.setBounds(77, 235, 70, 15);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(59, 134, 70, 15);
 		panel.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Prenom");
-		lblNewLabel_1_1.setBounds(399, 235, 70, 15);
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1_1.setBounds(59, 187, 70, 15);
 		panel.add(lblNewLabel_1_1);
 		
 		final Choice choice = new Choice();
-		choice.setBounds(399, 342, 175, 18);
+		choice.setBounds(274, 231, 175, 18);
 		choice.addItem("FISA");
 		choice.addItem("FISE");
 		panel.add(choice);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Filiere");
-		lblNewLabel_1_1_1.setBounds(399, 316, 70, 15);
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1_1_1.setBounds(59, 231, 70, 15);
 		panel.add(lblNewLabel_1_1_1);
 		
 		final JLabel label = new JLabel("");
@@ -104,15 +107,17 @@ public class AjouterEtudiantGUI {
 		panel.add(label);
 		
 		final JLabel lblGroupe = new JLabel("Groupe");
-		lblGroupe.setBounds(77, 316, 70, 15);
+		lblGroupe.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblGroupe.setBounds(59, 274, 70, 15);
 		panel.add(lblGroupe);
 		
 		textFieldGroupe = new JTextField();
-		textFieldGroupe.setBounds(77, 341, 175, 19);
+		textFieldGroupe.setBounds(274, 272, 175, 19);
 		panel.add(textFieldGroupe);
 		textFieldGroupe.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Ajouter");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Récupération des données
@@ -137,15 +142,17 @@ public class AjouterEtudiantGUI {
 		});
 		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.setBackground(new Color(255, 0, 0));
-		btnNewButton.setBounds(277, 418, 85, 21);
+		btnNewButton.setBounds(364, 370, 85, 21);
 		panel.add(btnNewButton);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setIcon(new ImageIcon("./images/logo_esigelec_boite-1.png"));
-		lblNewLabel_3.setBounds(79, -5, 574, 202);
+		lblNewLabel_3.setBounds(80, 0, 574, 202);
 		panel.add(lblNewLabel_3);
 		
 		JButton btnRetout = new JButton("Retour");
+		btnRetout.setBackground(new Color(255, 0, 0));
+		btnRetout.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnRetout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				@SuppressWarnings("unused")
@@ -153,17 +160,28 @@ public class AjouterEtudiantGUI {
 				frame.dispose();
 			}
 		});
-		btnRetout.setBounds(537, 368, 117, 25);
+		btnRetout.setBounds(286, 408, 117, 25);
 		panel.add(btnRetout);
 		
 		JLabel lblQuota = new JLabel("Quota");
-		lblQuota.setBounds(77, 372, 70, 15);
+		lblQuota.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblQuota.setBounds(59, 329, 70, 15);
 		panel.add(lblQuota);
 		
 		textFieldQuota = new JTextField();
-		textFieldQuota.setBounds(77, 399, 114, 19);
+		textFieldQuota.setBounds(274, 327, 175, 19);
 		panel.add(textFieldQuota);
 		textFieldQuota.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\ndeye\\Downloads\\petit logo esig.jpg"));
+		lblNewLabel_2.setBounds(0, 10, 139, 30);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\ndeye\\Downloads\\Ajout ens-etudiant.png"));
+		lblNewLabel_4.setBounds(492, 0, 214, 159);
+		panel.add(lblNewLabel_4);
 		
 		
 		
